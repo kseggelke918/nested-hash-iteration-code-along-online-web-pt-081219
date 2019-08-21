4b70e66619e4bc1cfc207d6ef819e3215acc6fb3
Katie Seggelke <katie.seggelke@gmail.com>
2019-08-21 20:25:32 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,9 +17,9 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury" 
-      contact_details_hash.each do |attribute, value|
+      contact_details_hash.each do |attribute, data|
         if attribute == favorite_ice_cream_flavors
-          value.shift
+          data.shift
         end 
       end 
     end 
